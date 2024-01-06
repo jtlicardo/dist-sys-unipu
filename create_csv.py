f91822925459ec53902caf58c832689385943e54
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-01-06 16:58:15 +0100

    Edit CSV args

--- create_csv.py
+++ create_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import argparse
+import os
 
 
 def create_sample_csv(file_name, num_rows, num_columns, range_start, range_end):
@@ -10,17 +11,19 @@ def create_sample_csv(file_name, num_rows, num_columns, range_start, range_end):
     }
     df = pd.DataFrame(data)
 
-    df.to_csv(file_name, index=False)
+    file_path = os.path.join("csv", file_name)
+
+    df.to_csv(file_path, index=False)
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_files', type=int, default=1)
-    parser.add_argument('--num_rows', type=int, default=5)
-    parser.add_argument('--num_columns', type=int, default=3)
+    parser.add_argument('--num_files', type=int, default=50)
+    parser.add_argument('--num_rows', type=int, default=50000)
+    parser.add_argument('--num_columns', type=int, default=10)
     parser.add_argument('--range_start', type=int, default=1)
-    parser.add_argument('--range_end', type=int, default=10)
+    parser.add_argument('--range_end', type=int, default=1000)
 
     args = parser.parse_args()
 
